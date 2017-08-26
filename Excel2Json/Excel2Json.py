c11259634ec1b668edc99b66c9ea9e3418735f50
peihua.lin <peihua.lin@mokun.net>
2017-08-26 16:24:05 +0800

    test buff.json success

--- Excel2Json/Excel2Json.py
+++ Excel2Json/Excel2Json.py
@@ -2,19 +2,20 @@
 # 作者：林培华
 # 策划的配置表格转为json格式存储
 # 后续可能的功能转为protobuf二进制文件同时生成.proto 后续可能的功能转为protobuf二进制文件同时生成对应的C#脚本
-
-import xlrd
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import xlrd
 import json
 import re
 
 class FieldItem:
     def __init__(self):
         self.name = ""
-        self.targets = "A"
+        self.targets = "a"
         self.type = ""
 
-fileOutput = open('buff.json', 'wb')
+fileOutput = open('buff.json', 'w')
 
 workbook = xlrd.open_workbook('buff.xlsx')
 booksheet = workbook.sheet_by_name('buff')
@@ -36,7 +37,7 @@ def myStr(num):
     elif type(num) == float or type(num) == int or type(num) == long:
         return str(num)
     else:
-        return num.encode('utf-8')
+        return num
 
 VectorPatten = re.compile('vector<(.*?)>', re.DOTALL | re.IGNORECASE)
 MapPatten = re.compile('map<(.*?),(.*?)>', re.DOTALL | re.IGNORECASE)
@@ -46,7 +47,6 @@ def parseCell(fieldItem, cell):
         if subType == 'string':
             return myStr(value)
         elif subType == 'int':
-            print(value)
             return int(value)
         elif subType == 'float':
             return float(value)
@@ -54,8 +54,8 @@ def parseCell(fieldItem, cell):
     fieldType = fieldItem.type
     m = VectorPatten.search(fieldType)
     if m != None:
-        subType = m.group(0)
-        split = cell.value.split(':')
+        subType = m.group(1)
+        split = cell.value.split('|')
         result = []
         for i in split:
             result.append(_innerParse(subType, i))
@@ -85,6 +85,10 @@ for rowIndex in range(4, rownumber):
             OutData[id] = {}
             OutData[id]['id'] = id
         elif cell.ctype == 1 or cell.ctype == 2:
+            if fieldItem.name == None or fieldItem.name == '':
+                continue
+            if fieldItem.targets == None or fieldItem.targets == '' or (fieldItem.targets.lower() != "a" and fieldItem.targets.lower() != "c"):
+                continue
             try:
                 parseCell(fieldItem, cell)
             except:
@@ -93,8 +97,8 @@ for rowIndex in range(4, rownumber):
                 OutData[id][fieldItem.name] = parseCell(fieldItem, cell)
             
 
-jsonOut = json.dumps(OutData,sort_keys=True, indent=4 ,separators=(',', ': '))
+jsonOut = json.dumps(OutData,sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': '))
 fileOutput.write(jsonOut)
-print(json.dumps(jsonOut))
+# print(json.dumps(jsonOut))
 
 
